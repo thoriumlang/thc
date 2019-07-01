52431982e2b2b2cfdbea68ab0b68f28aaa9eefd4
Christophe Pollet <cpollet@users.noreply.github.com>
2019-07-01 22:38:32 +0200

    Add error listener to ThoriumLexer instance used in unit tests

--- src/test/java/org/thoriumlang/antlr/lexer/Tokens.java
+++ src/test/java/org/thoriumlang/antlr/lexer/Tokens.java
@@ -15,13 +15,21 @@
  */
 package org.thoriumlang.antlr.lexer;
 
+import org.antlr.v4.runtime.ANTLRErrorListener;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Parser;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
 import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.atn.ATNConfigSet;
+import org.antlr.v4.runtime.dfa.DFA;
+import org.assertj.core.api.Assertions;
 import org.thoriumlang.antlr.ThoriumLexer;
 
 import java.util.ArrayList;
+import java.util.BitSet;
 import java.util.Collections;
 import java.util.List;
 
@@ -35,6 +43,27 @@ class Tokens {
     List<Token> parse() {
         CharStream cStream = CharStreams.fromString(string);
         ThoriumLexer lexer = new ThoriumLexer(cStream);
+        lexer.addErrorListener(new ANTLRErrorListener() {
+            @Override
+            public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
+                Assertions.fail("syntaxError");
+            }
+
+            @Override
+            public void reportAmbiguity(Parser recognizer, DFA dfa, int startIndex, int stopIndex, boolean exact, BitSet ambigAlts, ATNConfigSet configs) {
+                Assertions.fail("reportAmbiguity");
+            }
+
+            @Override
+            public void reportAttemptingFullContext(Parser recognizer, DFA dfa, int startIndex, int stopIndex, BitSet conflictingAlts, ATNConfigSet configs) {
+                Assertions.fail("reportAttemptingFullContext");
+            }
+
+            @Override
+            public void reportContextSensitivity(Parser recognizer, DFA dfa, int startIndex, int stopIndex, int prediction, ATNConfigSet configs) {
+                Assertions.fail("reportContextSensitivity");
+            }
+        });
         CommonTokenStream tokenStream = new CommonTokenStream(lexer);
         tokenStream.fill();
         return Collections.unmodifiableList(
