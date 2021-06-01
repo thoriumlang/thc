acba217ca38cae51c20484dc4df8228be0dea221
Christophe Pollet <cpollet@users.noreply.github.com>
2021-06-01 20:45:17 +0200

    Add AstHelper & SourceFilesHelper

---
+++ src/test/java/org/thoriumlang/compiler/testsupport/AstHelper.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2021 Christophe Pollet
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.thoriumlang.compiler.testsupport;
+
+import org.thoriumlang.compiler.ast.nodes.NodeIdGenerator;
+import org.thoriumlang.compiler.ast.nodes.Root;
+
+import java.net.URISyntaxException;
+
+public final class AstHelper {
+    private AstHelper() {
+    }
+
+    public static Root from(Class<?> c, String fileName) {
+        return from(c, fileName, new NodeIdGenerator());
+    }
+
+    public static Root from(Class<?> c, String fileName, NodeIdGenerator nodeIdGenerator) {
+        try {
+            return SourceFilesHelper.from(c, fileName)
+                    .sources()
+                    .get(0)
+                    .ast(nodeIdGenerator)
+                    .parse()
+                    .get();
+        }
+        catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
